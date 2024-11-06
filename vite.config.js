13464bb7e11820646227cddeebf7fd3057f41739
Warren Lee <juihsuanlee0303@gmail.com>
2024-11-06 19:31:05 +0800

    fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [vue()],
-    base: env.VITE_BASE_URL || '/',
+    base: '/personal-website/',
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url))
